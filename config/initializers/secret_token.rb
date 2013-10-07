27639e6bddc633a231cb59d2f08119f7d822fe8e
jiunjiun <quietmes@gmail.com>
2013-10-08 00:25:48 +0800

    Create Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ICCL::Application.config.secret_key_base = 'b0e62541bb93bdf7ebac670582bca610061cc7869740ea21ac1d8c27fee32cb6777c7248e4828ae3676eb139992849bac5ad3bbbfaee878d88380066c596c390'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ICCL::Application.config.session_store :cookie_store, key: '_ICCL_session'
