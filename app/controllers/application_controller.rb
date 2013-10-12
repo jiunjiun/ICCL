b20fb06e5c381d83a6237a9c2675d87f83a75ac8
jiunjiun <quietmes@gmail.com>
2013-10-12 23:35:18 +0800

    Add cancan Ability and authenticate if admin go admin layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,26 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # ---------------
+  # cancan Ability
+  def current_ability
+      @current_ability ||= ::Ability.new(User.find_by_id(session[:UserInfo][:id]))
+  end
+
+  # cancan has any err back root_path
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, :alert => exception.message
+  end
+  # ---------------
+
+
+  before_filter :authenticate
+
+  def authenticate
+    if params[:controller]["admin"]
+      # layout
+      render layout: "backend"
+    end
+  end
 end
