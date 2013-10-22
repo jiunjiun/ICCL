95f671f3b91f1696e67310fafd2630fc78396916
jiunjiun <quietmes@gmail.com>
2013-10-23 01:12:53 +0800

    update user take out commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-    # validates_presence_of :fb_id, :name, :email, :token
-    # validates :email, format: {:with=> /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i}
+    validates_presence_of :fb_id, :name, :email, :token
+    validates :email, format: {:with=> /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i}
 
-    # validates :classes, :uniqueness => { :allow_blank => true}
+    validates :classes, :uniqueness => { :allow_blank => true}
 end
