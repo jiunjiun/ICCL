0b998b555eeb9007da72da4a7b0cb717fac2d5cd
jiunjiun <quietmes@gmail.com>
2013-10-31 00:11:33 +0800

    Update user validates and index page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
     validates_presence_of :fb_id, :name, :email, :token
     validates :email, format: {:with=> /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i}
 
-    validates :classes, :inclusion => %w(master university)
+    # validates :classes, :inclusion => %w(master university)
 
     Classes = {:Master => 'master', :University=> 'university'}
     Classes.each{|key, val| scope key, :conditions => {:classes=> val, :role=> ['SU', 'UE']}, :order => 'name' }
