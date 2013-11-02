796481d2b1cf35a6986b6614aa19eb351080a0d3
jiunjiun <quietmes@gmail.com>
2013-11-02 18:35:21 +0800

    update root layout carousel for index page

--- app/helpers/index_helper.rb
+++ app/helpers/index_helper.rb
@@ -1,2 +1,7 @@
 module IndexHelper
+    def carousel
+        if params[:controller] == 'index' && params[:action] == 'index'
+            render :partial => "common/carousel"
+        end
+    end
 end
